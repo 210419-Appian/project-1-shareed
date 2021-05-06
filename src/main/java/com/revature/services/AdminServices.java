ee3fa9aec97d2a986677d3af72438547ff778e39
shareed <shannontlreed@gmail>
2021-05-05 20:16:11 -0700

    added admin servlet, services, and DAO

---
+++ src/main/java/com/revature/services/AdminServices.java
@@ -0,0 +1,17 @@
+package com.revature.services;
+
+import java.util.List;
+
+import com.revature.dao.EmployeeDAO;
+import com.revature.dao.EmployeeDAOImpl;
+import com.revature.models.User;
+
+public class AdminServices {
+	
+	private EmployeeDAO userDAO = new EmployeeDAOImpl();
+		
+		public List<User> getAllUsers() {
+			return userDAO.getAllUsers();
+		}
+
+}
---
+++ src/main/java/com/revature/servlets/AdminServlet.java
@@ -0,0 +1,35 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.User;
+import com.revature.services.EmployeeService;
+
+public class AdminServlet extends HttpServlet{
+	
+	
+	private EmployeeService employeeService = new EmployeeService();
+	private ObjectMapper om = new ObjectMapper();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<User> list = employeeService.getAllUsers();
+		
+		String json = om.writeValueAsString(list);
+		System.out.println(json);
+		PrintWriter pw = resp.getWriter();
+		pw.print(json);
+		resp.setStatus(200);
+		resp.setContentType("application/json");
+	
+	}
+
+}
